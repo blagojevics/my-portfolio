fb2126423b80c0487f1793a13a0522f50c89d0b2
Stefan Blagojevic <blagojevics710@gmail.com>
2025-09-22 15:55:40 +0200

    more style fixes

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -5,20 +5,20 @@ export default function AboutSection() {
   return (
     <section
       id="about"
-      className="w-full max-w-4xl mx-auto py-10 px-0 sm:px-6 flex flex-col bg-[var(--background-color)] transition-colors duration-300"
+      className="w-full max-w-4xl mx-auto 2xl:px-0 py-10  sm:px-6 flex flex-col bg-[var(--background-color)] transition-colors duration-300"
     >
       <div className="grid md:grid-cols-2 gap-8 md:gap-12 items-start">
         {/* Left: Name & Profession */}
-        <div className="flex flex-col gap-2.5">
-          <h1 className="text-4xl sm:text-5xl font-extrabold text-[var(--text-color)] mb-1 flex flex-col gap-1">
+        <div className="flex flex-col gap-2 ">
+          <h1 className="ml-2 text-4xl sm:text-5xl font-extrabold text-[var(--text-color)] mb-1 flex flex-col gap-1">
             Hi, I&apos;m
             <span className="text-[var(--gradient-color)]">Stefan</span>
             <span className="text-[var(--gradient-color)]">Blagojević</span>
           </h1>
-          <h2 className="text-[1.2rem] sm:text-2xl font-semibold text-[var(--text-color)] mb-2">
+          <h2 className="ml-2 text-[1.2rem] sm:text-2xl font-semibold text-[var(--text-color)] mb-2">
             Frontend Developer
           </h2>
-          <h3 className="text-lg font-semibold text-[var(--text-color)] flex gap-1 items-center">
+          <h3 className=" ml-2 text-lg font-semibold text-[var(--text-color)] flex gap-1 items-center">
             <FaMapMarkerAlt
               className="text-2xl"
               style={{
